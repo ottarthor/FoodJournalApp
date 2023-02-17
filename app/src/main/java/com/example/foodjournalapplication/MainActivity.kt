388a39b8a05d48c20dfde542d672644247012ead
ottarthor <ottarthor@gmail.com>
2023-02-17 11:48:40 +0000

    network manager

--- app/src/main/java/com/example/foodjournalapplication/MainActivity.kt
+++ app/src/main/java/com/example/foodjournalapplication/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.foodjournalapplication
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.text.format.DateFormat
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.foodjournalapplication.NetworkManager.NetworkCallback
+import com.example.foodjournalapplication.NetworkManager.NetworkManager
 import com.example.foodjournalapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var test: String
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d("hæ","sdmflkmdsf");
+        val networkManager = NetworkManager.getInstance(this);
+        networkManager.getTest(object : NetworkCallback<String> {
+            override fun onSuccess(result: String) {
+                    test = result;
+                    Log.d("SDFNJSDFN", test);
+            }
+
+            override fun onFailure(errorString: String?) {
+                Log.d("TEST TOTODLSMKM","EPIC Fail");
+            }
+        })
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
